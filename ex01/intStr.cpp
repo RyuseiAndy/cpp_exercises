15bdc91c15c8053c8c925886acf3e4474e24919e
Andy <83407832+RyuseiAndy@users.noreply.github.com>
2021-10-07 17:09:38 +0900

    add intStr

--- ex01/intStr.cpp
+++ ex01/intStr.cpp
@@ -1,17 +1,44 @@
 #include <intStr.h>
 #include <iostream>
-#include <memory>
 
-intToStr::intStr(int num)
-:num_(num)
-{}
+char* intToStr(int num)
+{
+    int len=0;
+    int intVal = num;
+    while(intVal>=0.1)
+    {
+        len++;
+        intVal /= 10;
+    }
+    count<<len<<endl;
+    int intVal = len;
+    char *ans = (char *)malloc(1*len);
 
-char* intStr::intToStr(int)
-{
-   int* S = (int*)malloc(sizeof(int) * num_);
-    
+    int arr[len];
+    int val;
+    for (int i = 0; i < len; i++)
+    {
+        arr[len-i-1] = num % 10;
+        num /= 10;
+    }
+    for(int i =0; i < len; i++){
+        ans[i] = '0' + arr[i];
+    }
+
+    return ans;
 }
-int intStr::strToInt(char*)
-{
+
+int strToInt(char　*str){
+    int num = 0;
     
+    while(*str != '\0'){
+        num += *str - 48;
+        num *= 10;
+        str++;
+    }
+    
+    num /= 10;
+    
+    return num;
 }
+
