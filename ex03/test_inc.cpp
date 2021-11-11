c474c1aa20562a8bf5be2d2414bc7fa10d8aa94e
Ryusei <83407832+RyuseiAndy@users.noreply.github.com>
2021-11-11 17:15:29 +0900

    Add files via upload

--- ex03/test_inc.cpp
+++ ex03/test_inc.cpp
@@ -1,19 +1,21 @@
 // test_inc.cpp
 #include <cassert>
 // COMPLETE Include any additional necessary headers here
-#include "inc.h"
 #include "inc.cpp"
 
 int main(void) {
   double a = 1.0;
+  //std::cout<<a<<std::endl;
   inc(a); 
+  //std::cout<<a<<std::endl;
   assert(a == 2.0);
   inc(a); 
   assert(a == 3.0);
-  //std::cout<<a<<std::endl;
 
   a = 1.0;
+  //std::cout<<a<<std::endl;
   inc(&a); 
+  //std::cout<<a<<std::endl;
   assert(a == 2.0);
   inc(&a); 
   assert(a == 3.0);
