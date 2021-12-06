8f40196de8d7cec947869aa05f6d78a8727d2230
RyuseiAndy <ryuseiandy06@gmail.com>
2021-12-06 17:15:37 +0900

    fix file q1

--- ex10/q1/EvaluationFP.cpp
+++ ex10/q1/EvaluationFP.cpp
@@ -9,41 +9,41 @@ using namespace std;
 
 double evaluate(string data) {
     stack<double> operand;
-    stack<string> operat;
+    stack<string> opetating;
     stringstream ss(data);
-    string d;
+    string x;
 
-    while(ss >> d) {
-        if(d == "+" || d == "-" || d == "*" || d == "/"){
-            operat.push(d);
+    while(ss >> x) {
+        if(x == "+" || x == "-" || x == "*" || x == "/"){
+            opetating.push(x);
         }
-        else if(d == "(" ) {
+        else if(x == "(" ) {
         }
-        else if(d == ")" ) {
+        else if(x == ")" ) {
             double a1 = operand.top();
             operand.pop();
             double a2 = operand.top();
             operand.pop();
-            string a3 = operat.top();
+            string a3 = opetating.top();
             if(a3 == "+") {
-                operat.pop();
+                opetating.pop();
                 double b1 = a2 + a1;
                 operand.push(b1);
             } else if(a3 == "-") {
-                operat.pop();
+                opetating.pop();
                 double b2 = a2 - a1;
                 operand.push(b2);
             } else if(a3 == "*") {
-                operat.pop();
+                opetating.pop();
                 double b3 = a2 * a1;
                 operand.push(b3);
             } else if(a3 == "/") {
-                operat.pop();
+                opetating.pop();
                 double b4 = a1 / a2;
                 operand.push(b4);
             }
         }
-        else operand.push(stod(d));
+        else operand.push(stod(x));
     }
     return operand.top();
 }
@@ -52,8 +52,6 @@ int main() {
     string data1 = "3 + 5 * 6 - 7 * ( 8 + 5 )";
     string data2  = "25.0 * 12.0 / 5.0 - 8.0";
     string data3 = "1 + 2 * 3 - 4 * ( 5 + 6 )";
-
-    cout << fixed << setprecision(1);
 
     cout << "Input: " << data1 << endl;
     cout << "output: " << evaluate(data1) << endl;
