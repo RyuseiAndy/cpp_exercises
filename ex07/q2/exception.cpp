db8cbed4b276b5ddc70cc6a1c4da56e9f1df536e
RyuseiAndy <ryuseiandy06@gmail.com>
2021-12-07 13:18:18 +0900

    fix file q2

--- ex07/q2/exception.cpp
+++ ex07/q2/exception.cpp
@@ -1,47 +1,60 @@
-// exception.cpp
 #include <stdexcept>
 #include <cstdio>
 #include <iostream>
 
 using namespace std;
 
-class LogFile{
+class LogFile {
     FILE* logfile;
-
-    void fputs(const char *s,FILE *logfile){}
-
+    public:
+        LogFile(const char* filename) {
+            logfile = fopen(filename, "w+");
+            if (!logfile) {
+                throw runtime_error("Failed to open file");
+            }
+        }
+        ~LogFile() {
+            fclose(logfile);
+        }
+        void fputs(const char* input) {
+            const int fil1 = ::fputs(input, logfile);
+            if (fil1 < 0) {
+                throw runtime_error("Failed to write data on file!");
+            }
+            const int fil2 = ::fputs("\n", logfile);
+            if (fil2 < 0) {
+                throw runtime_error("Failed to write data on file!");
+            }
+        }
 };
 
 void doSomeComputation() {   
- throw runtime_error("failure during doing some computation");
+    if ((1 + 1) != 2) {
+        throw runtime_error("failure during doing some computation");
+    }
 }
 
 void example() {
- FILE* logfile = fopen("logfile.txt", "w+");
- if (!logfile) {
-   throw runtime_error("failed to open file");
- }
+    LogFile* logfile = new LogFile("logfile.txt");
 
- fputs("log - 1", logfile);
+    logfile->fputs("log - 1");
 
- // Call a function that performs some computation and may throw 
- // an exception
-
-try{
-    doSomeComputation();
-    }catch(runtime_error& e){
-        cout << e.what() << endl;
+    // Call a function that performs some computation and may throw 
+    // an exception
+    try {
+        doSomeComputation();
+    } catch(const runtime_error& e) {
+        cerr << "exception caught" << endl;
     }
 
- fputs("log - 2", logfile);
-    
- cout << "closing logfile" << endl;
- fclose(logfile);
+    logfile->fputs("log - 2");
+
+    cout << "closing logfile" << endl;
 }
 
 int main(void) {
- cout << "Calling example()" << endl;
- example();
- cout << "After calling example()" << endl;
- return 0;
+    cout << "Calling example()" << endl;
+    example();
+    cout << "After calling example()" << endl;
+    return 0;
 }
